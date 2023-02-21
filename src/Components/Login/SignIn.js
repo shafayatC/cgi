301740260e29efc38b54ab7c3f39cbf7090bcd98
Faizul-Hassan-2015 <86608620+Faizul-Hassan-2015@users.noreply.github.com>
2023-02-21 16:00:24 +0600

    add navbar

---
+++ src/Components/Login/SignIn.js
@@ -0,0 +1,105 @@
+import React from "react";
+// import { FaFacebookSquare, FaGoogle } from "react-icons/fa";
+import { Link } from "react-router-dom";
+// import login from "../../images/login.jpg";
+
+const SignIn = () => {
+  return (
+    <section>
+      <div class="px-6 h-full text-gray-800">
+        <div class="flex xl:justify-center lg:justify-center justify-center items-center mt-20 flex-wrap h-full g-6">
+          {/* <div class="grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 mb-12 md:mb-0">
+                <img src={login} class="w-full rounded-lg" alt="Sample image" />
+              </div> */}
+          <div class="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
+            <form>
+              <div class="flex flex-row items-center justify-center lg:justify-start">
+                <p class="text-lg mb-0 mr-4">Sign in with</p>
+                <button
+                  type="button"
+                  data-mdb-ripple="true"
+                  data-mdb-ripple-color="light"
+                  class="inline-block p-3 bg-orange-400 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out mx-1"
+                >
+                  {/* <FaFacebookSquare className="h-4 w-4" /> */}
+                </button>
+
+                <button
+                  type="button"
+                  data-mdb-ripple="true"
+                  data-mdb-ripple-color="light"
+                  class="inline-block p-3 bg-orange-400 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out mx-1"
+                >
+                  {/* <FaGoogle className="h-4 w-4"></FaGoogle> */}
+                </button>
+              </div>
+
+              <div class="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5">
+                <p class="text-center font-semibold mx-4 mb-0">Or</p>
+              </div>
+
+              <div class="mb-6">
+                <input
+                  type="text"
+                  class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                  id="exampleFormControlInput2"
+                  placeholder="Email address"
+                />
+              </div>
+
+              <div class="mb-6">
+                <input
+                  type="password"
+                  class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                  id="exampleFormControlInput2"
+                  placeholder="Password"
+                />
+              </div>
+
+              <div class="flex justify-between items-center mb-6">
+                <div class="form-group form-check">
+                  <input
+                    type="checkbox"
+                    class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-orange-400 checked:border-orange-400 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                    id="exampleCheck2"
+                  />
+                  <label
+                    class="form-check-label inline-block text-gray-800"
+                    for="exampleCheck2"
+                  >
+                    Remember me
+                  </label>
+                </div>
+                <a href="#!" class="text-gray-800 hover:text-red-600">
+                  Forgot password?
+                </a>
+              </div>
+
+              <div class="text-center lg:text-left">
+                <button
+                  type="button"
+                  class="inline-block px-7 py-3 bg-orange-400 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-lime-400 hover:shadow-lg focus:bg-lime-400 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                >
+                  Login
+                </button>
+                <p class="text-sm font-semibold mt-2 pt-1  mb-0">
+                  Don't have an account?
+                  <Link to="/signup">
+                    <a
+                      href="#!"
+                      class="text-red-600 hover:text-lime-400 focus:text-red-700 transition duration-200 ease-in-out"
+                    >
+                      Sign up
+                    </a>
+                  </Link>
+                </p>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SignIn;
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,85 @@
+import React from "react";
+import { useState } from "react";
+import { Drawer } from "antd";
+import { HomeOutlined, MenuOutlined } from "@ant-design/icons";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  const showDrawer = () => {
+    setOpen(true);
+  };
+
+  const onClose = () => {
+    setOpen(false);
+  };
+  return (
+    <div className="bg">
+      <h4 className="font-bold text-3xl ml-5 text-green-700">
+        {" "}
+        The Kow Company
+      </h4>
+      <div className="flex items-center  mt-2">
+        <button
+          className="mr-3 border-green-600 border-2 h-12 w-12 rounded-full"
+          onClick={showDrawer}
+        >
+          <MenuOutlined className="h-4 w-5 font-medium " />
+        </button>
+        <button className="mr-8  border-green-600 border-2 h-12 w-12 rounded-full">
+          <HomeOutlined className="h-4 w-5 font-medium " />
+        </button>
+      </div>
+      <Drawer onClose={onClose} open={open}>
+        <ul className="nav-ul">
+          <li>
+            <Link onClick={onclose} to="home">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link onClick={onclose} to="">
+              Services
+            </Link>
+          </li>
+          <li>
+            <Link onClick={onclose} to="">
+              Contact
+            </Link>
+          </li>
+          <li>
+            <Link onClick={onclose} to="">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="sign-in"
+              onClick={onClose}
+              className="font-semibold hover:text-white "
+            >
+              <button className=" hover:bg-green-500 w-80 text-left hover:text-white rounded-md p-1 ">
+                Sign in
+              </button>
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="sign-up"
+              onClick={onclose}
+              className="font-semibold hover:text-white"
+            >
+              <button className=" hover:bg-green-500 w-80 text-left hover:text-white rounded-md p-1">
+                Sign up
+              </button>
+            </Link>
+          </li>
+        </ul>
+      </Drawer>
+    </div>
+  );
+};
+
+export default Navbar;
