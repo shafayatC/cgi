145fc15f7c1e2c74070996043721114e53bf66ee
Faizul-Hassan-2015 <86608620+Faizul-Hassan-2015@users.noreply.github.com>
2023-02-23 20:44:32 +0600

    bg remove and add image button

--- src/Components/OurCase/OurCase.js
+++ src/Components/OurCase/OurCase.js
@@ -10,36 +10,79 @@ import p8 from "./images/8.png";
 
 const OurCase = () => {
   return (
-    <div className="pt-10 pb-10">
+    <div className=" pt-10 pb-10">
       <div>
         <p className="text-white  text-center border-b-2 border-b-shade-color text-4xl">
           Our cases
         </p>
       </div>
-      <div className="grid lg:grid-cols-4 mx-20 gap-4 mt-10  justify-center">
+      <div className="grid lg:grid-cols-4 mx-20 gap-4 mt-16  justify-center">
+        <div className="relative block bg-gray-900 w-64 group">
+          <div className="">
+            <img
+              src={p1}
+              alt="logo"
+              className="h-60 w-64 absolute inset-0  group-hover:opacity-80 hover:border-r-4 hover:border-t-4  hover:border-shade-color hover:scale-110 transition duration-300 ease-in-out"
+            />
+          </div>
+          <div className="relative top-28 left-20">
+            <div className="transition-all transform translate-y-4  opacity-0 group-hover:opacity-100 group-hover:translate-y-0">
+              <p className="text-white text-xl mb-2">Ladies Bag</p>
+              <button className="px-4 py-2 text-sm text-white  bg-shade-color">
+                View More
+              </button>
+            </div>
+          </div>
+        </div>
+
         <div>
-          <img className="h-60 w-64" src={p1} alt="" />
+          <img
+            className="h-60 w-64 hover:border-r-4 hover:border-t-4 hover:border-shade-color hover:scale-110 transition duration-300 ease-in-out"
+            src={p2}
+            alt=""
+          />
         </div>
         <div>
-          <img className="h-60 w-64" src={p2} alt="" />
+          <img
+            className="h-60 w-64 hover:border-r-4 hover:border-t-4 hover:border-shade-color hover:scale-110 transition duration-300 ease-in-out"
+            src={p3}
+            alt=""
+          />
         </div>
         <div>
-          <img className="h-60 w-64" src={p3} alt="" />
+          <img
+            className="h-60 w-64 hover:border-r-4 hover:border-t-4 hover:border-shade-color hover:scale-110 transition duration-300 ease-in-out"
+            src={p4}
+            alt=""
+          />
         </div>
         <div>
-          <img className="h-60 w-64" src={p4} alt="" />
+          <img
+            className="h-60 w-64 hover:border-r-4 hover:border-t-4 hover:border-shade-color hover:scale-110 transition duration-300 ease-in-out"
+            src={p5}
+            alt=""
+          />
         </div>
         <div>
-          <img className="h-60 w-64" src={p5} alt="" />
+          <img
+            className="h-60 w-64 hover:border-r-4 hover:border-t-4 hover:border-shade-color hover:scale-110 transition duration-300 ease-in-out"
+            src={p6}
+            alt=""
+          />
         </div>
         <div>
-          <img className="h-60 w-64" src={p6} alt="" />
+          <img
+            className="h-60 w-64 hover:border-r-4 hover:border-t-4 hover:border-shade-color hover:scale-110 transition duration-300 ease-in-out"
+            src={p7}
+            alt=""
+          />
         </div>
         <div>
-          <img className="h-60 w-64" src={p7} alt="" />
-        </div>
-        <div>
-          <img className="h-60 w-64" src={p8} alt="" />
+          <img
+            className="h-60 w-64 hover:border-r-4 hover:border-t-4 hover:border-shade-color hover:scale-110 transition duration-300 ease-in-out"
+            src={p8}
+            alt=""
+          />
         </div>
       </div>
     </div>
