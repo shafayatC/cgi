d36cffb98b50ed466df7180758ddb479579b8722
shafayatC <shafayatplay@gmail.com>
2023-02-23 18:47:54 +0600

    update homepage

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React from 'react';
+import './style.css'; 
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div className='container m-auto'>
+      <img src={require('./img/kshape.png')} className="h-screen fixed z-0 top-0 col-span-1"/>
+      <div className='home_right col-span-1'>
+        <h3>3D PRODUCT</h3>
+        <h2>MODELING</h2>
+        <button>Get Started</button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -61,87 +61,87 @@ const Navbar = () => {
   ];
 
   return (
-    <div className="flex items-center justify-between">
-      <h4 className="font-bold text-3xl ml-5 text-green-700">
-        The Kow Company
-      </h4>
-      <div className="flex items-center  mt-2">
-        <button
-          className="mr-3 border-green-600 border-2 h-12 w-12 rounded-full"
-          onClick={showDrawer}
-        >
-          <MenuOutlined className="h-4 w-5 font-medium " />
-        </button>
-        <button className="mr-8  border-green-600 border-2 h-12 w-12 rounded-full">
-          <HomeOutlined className="h-4 w-5 font-medium " />
-        </button>
-      </div>
-      <Drawer onClose={onClose} open={open}>
-        <ul className="nav-ul">
-          <li>
-            <Link className="font-semibold" onClick={onclose} to="home">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Dropdown
-              menu={{
-                items,
-              }}
-              trigger={["click"]}
-              className="bg-black"
-            >
-              <Link
-                className="font-semibold hover:text-white "
-                onClick={(e) => e.preventDefault()}
+    <div className="container mx-auto pt-5 relative z-10">
+      <div className="flex items-center justify-between">
+        <a href="#"><img className="logo w-[100px]" src="img/logo.png" /></a>
+        <div className="menu_wrap flex items-center  mt-2">
+          <button
+            className="menu mr-3 border-green-600 border-2 h-12 w-12 rounded-full"
+            onClick={showDrawer}
+          >
+            <MenuOutlined className="h-4 w-5 font-medium " />
+          </button>
+          <button className="menu mr-8  border-green-600 border-2 h-12 w-12 rounded-full">
+            <HomeOutlined className="h-4 w-5 font-medium " />
+          </button>
+        </div>
+        <Drawer onClose={onClose} open={open}>
+          <ul className="nav-ul">
+            <li>
+              <Link className="font-semibold" onClick={onclose} to="home">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Dropdown
+                menu={{
+                  items,
+                }}
+                trigger={["click"]}
+                className="bg-black"
               >
-                <button className="text-2xl flex items-center  hover:bg-green-500 w-full text-left hover:text-white rounded-md p-1 font-semibold">
-                  Services
-                  <CaretDownOutlined className="  ml-4 mt-1" />
+                <Link
+                  className="font-semibold hover:text-white "
+                  onClick={(e) => e.preventDefault()}
+                >
+                  <button className="text-2xl flex items-center  hover:bg-green-500 w-full text-left hover:text-white rounded-md p-1 font-semibold">
+                    Services
+                    <CaretDownOutlined className="  ml-4 mt-1" />
+                  </button>
+                </Link>
+              </Dropdown>
+            </li>
+            <li>
+              <Link className="font-semibold " onClick={onclose} to="contact">
+                Contact
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="about"
+                onClick={onclose}
+                className="font-semibold hover:text-white"
+              >
+                <button className=" hover:bg-green-500 w-full text-left hover:text-white rounded-md p-1">
+                  About
                 </button>
               </Link>
-            </Dropdown>
-          </li>
-          <li>
-            <Link className="font-semibold " onClick={onclose} to="contact">
-              Contact
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="about"
-              onClick={onclose}
-              className="font-semibold hover:text-white"
-            >
-              <button className=" hover:bg-green-500 w-full text-left hover:text-white rounded-md p-1">
-                About
-              </button>
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="sign-in"
-              onClick={onClose}
-              className="font-semibold hover:text-white "
-            >
-              <button className=" hover:bg-green-500 w-full text-left hover:text-white rounded-md p-1 ">
-                Sign in
-              </button>
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="sign-up"
-              onClick={onclose}
-              className="font-semibold hover:text-white"
-            >
-              <button className=" hover:bg-green-500 w-full text-left hover:text-white rounded-md p-1">
-                Sign up
-              </button>
-            </Link>
-          </li>
-        </ul>
-      </Drawer>
+            </li>
+            <li>
+              <Link
+                to="sign-in"
+                onClick={onClose}
+                className="font-semibold hover:text-white "
+              >
+                <button className=" hover:bg-green-500 w-full text-left hover:text-white rounded-md p-1 ">
+                  Sign in
+                </button>
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="sign-up"
+                onClick={onclose}
+                className="font-semibold hover:text-white"
+              >
+                <button className=" hover:bg-green-500 w-full text-left hover:text-white rounded-md p-1">
+                  Sign up
+                </button>
+              </Link>
+            </li>
+          </ul>
+        </Drawer>
+      </div>
     </div>
   );
 };
