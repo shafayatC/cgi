10ac2304cb049e7c40eb66f9c2dcfc43b3e35f84
Faizul-Hassan-2015 <86608620+Faizul-Hassan-2015@users.noreply.github.com>
2023-02-27 18:52:41 +0600

    add some changes on footer

--- src/Components/ProductVideo/ProductVideo.js
+++ src/Components/ProductVideo/ProductVideo.js
@@ -3,8 +3,8 @@ import video from "./video/production.mp4";
 
 const ProductVideo = () => {
   return (
-    <div className="flex justify-center">
-      <video className="mt-20 mb-20" width="600" height="450" controls>
+    <div className="flex container mx-auto justify-center">
+      <video className="mt-20 mb-20" width="90%" height="450" controls>
         <source src={video} type="video/mp4" />
       </video>
     </div>
