f27247d6deae180dd1ee823179ae1b7a50891f86
Faizul-Hassan-2015 <86608620+Faizul-Hassan-2015@users.noreply.github.com>
2023-02-27 13:22:36 +0600

    dynamic our case component and add animation

--- src/Components/OurCase/OurCase.js
+++ src/Components/OurCase/OurCase.js
@@ -1,90 +1,58 @@
 import React from "react";
-import p1 from "./images/1.png";
-import p2 from "./images/2.png";
-import p3 from "./images/3.png";
-import p4 from "./images/4.png";
-import p5 from "./images/5.png";
-import p6 from "./images/6.png";
-import p7 from "./images/7.png";
-import p8 from "./images/8.png";
 import "./style.css";
+import p1 from "../OurCase/images/1.png";
+import p2 from "../OurCase/images/2.png";
+import p3 from "../OurCase/images/3.png";
+import p4 from "../OurCase/images/4.png";
+import p5 from "../OurCase/images/5.png";
+import p6 from "../OurCase/images/6.png";
+import p7 from "../OurCase/images/7.png";
+import p8 from "../OurCase/images/8.png";
 
-const OurCase = () => {
+const Case = () => {
+  const CaseImage = [
+    {
+      img: p1,
+    },
+    {
+      img: p2,
+    },
+    {
+      img: p3,
+    },
+    {
+      img: p4,
+    },
+    {
+      img: p5,
+    },
+    {
+      img: p6,
+    },
+    {
+      img: p7,
+    },
+    {
+      img: p8,
+    },
+  ];
   return (
-    <div className=" pt-10 container mx-auto pb-10">
+    <div className="pt-10 container mx-auto pb-9">
       <div className=" heading_1  ">
         <h2>Our cases</h2>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-4 mx-20 gap-3 mt-16  justify-items-center">
-        <div className="relative block bg-gray-900 w-64 group">
-          <div className="">
-            <img
-              src={p1}
-              alt="logo"
-              className="h-60 w-64 absolute inset-0  group-hover:opacity-80 hover:border-r-4 hover:border-t-4  hover:border-shade-color hover:scale-110 transition duration-300 ease-in-out"
-            />
-          </div>
-          <div className="relative top-28 left-20">
-            <div className="transition-all transform translate-y-4  opacity-0 group-hover:opacity-100 group-hover:translate-y-0">
-              <p className="text-white text-xl mb-2">Ladies Bag</p>
-              <button className="px-4 py-2 text-sm text-white  bg-shade-color">
-                View More
-              </button>
+        {CaseImage.map((data) => (
+          <div>
+            <div className="img-container">
+              <img className="img-bag" src={data.img} alt="" />
+              <button className="button">View More</button>
             </div>
           </div>
-        </div>
-        <div>
-          <img
-            className="h-60 w-64 hover:border-r-4 hover:border-t-4 hover:border-shade-color hover:scale-110 transition duration-300 ease-in-out"
-            src={p2}
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            className="h-60 w-64 hover:border-r-4 hover:border-t-4 hover:border-shade-color hover:scale-110 transition duration-300 ease-in-out"
-            src={p3}
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            className="h-60 w-64 hover:border-r-4 hover:border-t-4 hover:border-shade-color hover:scale-110 transition duration-300 ease-in-out"
-            src={p4}
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            className="h-60 w-64 hover:border-r-4 hover:border-t-4 hover:border-shade-color hover:scale-110 transition duration-300 ease-in-out"
-            src={p5}
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            className="h-60 w-64 hover:border-r-4 hover:border-t-4 hover:border-shade-color hover:scale-110 transition duration-300 ease-in-out"
-            src={p6}
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            className="h-60 w-64 hover:border-r-4 hover:border-t-4 hover:border-shade-color hover:scale-110 transition duration-300 ease-in-out"
-            src={p7}
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            className="h-60 w-64 hover:border-r-4 hover:border-t-4 hover:border-shade-color hover:scale-110 transition duration-300 ease-in-out"
-            src={p8}
-            alt=""
-          />
-        </div>
+        ))}
       </div>
     </div>
   );
 };
 
-export default OurCase;
+export default Case;
