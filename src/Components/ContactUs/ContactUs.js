f63a333702ea08ce66fafee57599e04c3f88948e
Faizul-Hassan-2015 <86608620+Faizul-Hassan-2015@users.noreply.github.com>
2023-02-27 16:20:41 +0600

    add map and change some font size

--- src/Components/ContactUs/ContactUs.js
+++ src/Components/ContactUs/ContactUs.js
@@ -6,12 +6,12 @@ const ContactUs = () => {
       <div className="mt-20 heading_3 mb-20">
         <h2>Discuss your ambitions with us</h2>
         <div className="flex flex-col mt-10 items-center">
-          <p className=" text-white  ">
+          <p className=" text-white text-lg ">
             Use our knowledge to get your organization ready for the future.{" "}
             <br />
             Contact our experts now for an introductory meeting.
           </p>
-          <button className="bg-shade-color px-4 py-1 mt-6 font-semibold text-white">
+          <button className="bg-shade-color px-4 py-1 mt-6 text-lg font-semibold text-white">
             Contact Us
           </button>
         </div>
